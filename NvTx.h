e881f1ec7ae780a5b3386fcedb63b5a309e2488f
Oleg Volkov <olegv@parallels.com>
2019-12-31 01:06:23 +0300

    Assign addresses to storage location automatically

--- NvTx.h
+++ NvTx.h
@@ -1,5 +1,17 @@
 #pragma once
 
+/*
+ Transactional non-volatile storage uses EEPROM for reliably storing
+ client's data. It provides the following useful properties:
+ - client will not read the data it have never written
+ - client will not read the data corrupted for any reason
+ - the data update will succeed either entirely or not at all, in the latter
+   case old data will be read
+ - the data is bound to the semantic tag (the string) provided by the client so
+   the data originally bound to the different tag will not be written to avoid
+   erroneous interpretation of the semantically different data
+*/
+
 // Read the stored value. Returns true if the value was successfully read. This function should always be called
 // before nv_tx_put to format storage even in case you are not interested in the current value.
 bool nv_tx_get(
@@ -18,8 +30,24 @@ void nv_tx_put(
 	);
 
 // The following helper macro get variable name as the parameter. The only other parameter required is EEPROM base address.
-#define NvTxGet(var, addr) nv_tx_get(#var, &var, sizeof(var), addr)
-#define NvTxPut(var, addr) nv_tx_put(#var, &var, sizeof(var), addr)
+#define NvTxGetAt(var, addr) nv_tx_get(#var, &var, sizeof(var), addr)
+#define NvTxPutAt(var, addr) nv_tx_put(#var, &var, sizeof(var), addr)
 
 // Calculates the amount of storage is takes on EEPROM to save the value of the variable
 #define NvTxSize(var) (2 * (4 + sizeof(var)))
+
+//
+// The following set of macro provide the means for automatically assigning addresses to storage locations
+//
+
+// Declare variable's storage address explicitly.
+// Typically used for declaring first address of the sequence of storage locations.
+#define NvPlace(var, addr) enum {var##_addr = addr}
+// Assign the address to 'var's storage next to the 'prev's storage location
+#define NvAfter(var, prev) enum {var##_addr = prev##_addr + NvTxSize(prev)}
+// Get the address assigned to the variable's storage location
+#define NvAddr(var) var##_addr
+
+// Get/put variable using the previously declared address
+#define NvTxGet(var) NvTxGetAt(var, NvAddr(var))
+#define NvTxPut(var) NvTxPutAt(var, NvAddr(var))
--- examples/NvTx_start_cnt/NvTx_start_cnt.ino
+++ examples/NvTx_start_cnt/NvTx_start_cnt.ino
@@ -8,22 +8,25 @@
 unsigned cnt1;
 uint8_t  cnt2;
 uint8_t  cnt3;
-
-#define ADDR1 0x10
-#define ADDR2 (ADDR1 + NvTxSize(cnt1))
+//
+// Assign addresses to storage locations
+// where cnt1 and cnt2 will be permanently stored
+//
+NvPlace(cnt1, 0x10); // place at explicitly defined address
+NvAfter(cnt2, cnt1); // place cnt2 right after cnt1
 
 void setup()
 {
 	Serial.begin(9600);
 
-	if (NvTxGet(cnt1, ADDR1))
+	if (NvTxGet(cnt1))
 		++cnt1;
 	else
 		cnt1 = 1;
 
-	NvTxPut(cnt1, ADDR1);
+	NvTxPut(cnt1);
 
-	if (NvTxGet(cnt2, ADDR2))
+	if (NvTxGet(cnt2))
 		++cnt2;
 	else
 		cnt2 = 1;
@@ -32,19 +35,19 @@ void setup()
 		// Simulate bad transaction.
 		// The new value will not be stored.
 		// Previous value will be read instead.
-		NvTxPut(cnt3, ADDR2);
+		NvTxPutAt(cnt3, NvAddr(cnt2));
 	else
-		NvTxPut(cnt2, ADDR2);
+		NvTxPut(cnt2);
 	
 }
 
 void loop()
 {	
-	if (NvTxGet(cnt1, ADDR1)) {
+	if (NvTxGet(cnt1)) {
 		Serial.print("cnt1=");
 		Serial.println(cnt1);
 	}
-	if (NvTxGet(cnt2, ADDR2)) {
+	if (NvTxGet(cnt2)) {
 		Serial.print("cnt2=");
 		Serial.println(cnt2);
 	}
